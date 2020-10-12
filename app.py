9b102751ba20539c25326afe99d339cbc2a2804b
SeanFromOhio <seanohioroberts@gmail.com>
2020-10-12 18:46:41 -0400

    Contact form now sends emails to me with a subject and the sender email and the associated message using the gmail server which allows up to 100 emails a day.

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ from flask import Flask, render_template, request, redirect
 from flask_mail import Mail, Message
 
 app = Flask(__name__)
+app.config.update(
+    # Email Settings
+    MAIL_SERVER="smtp.gmail.com",
+    MAIL_PORT=465,
+    MAIL_USE_SSL=True,
+    # SAVE AS AN ENVIRONMENTAL VARIABLE SO NO ONE STEAL THIS INFO!!!
+    MAIL_USERNAME="seanohioroberts@gmail.com",
+    MAIL_PASSWORD="Qazwsxujm77"
+)
 mail = Mail(app)
 
 
@@ -15,21 +24,24 @@ def resume():
     return render_template("resume.html")
 
 
-@app.route("/contact", methods=["POST"])
+@app.route("/contact", methods=["POST", "GET"])
 def contact():
     if request.method == "POST":
         name = request.form["name"]
         email = request.form["email"]
         subject = request.form["subject"]
         message = request.form["message"]
+        subject_str = ("*Portfolio* " + name + " - " + subject)
+        body_str = ("You've received a new contact from: " + email + "\n\n Message: \n\n" + message)
 
         msg = Message(
-            message,
-            subject="*Portfolio*" + name + "-" + subject,
+            body=body_str,
+            subject=subject_str,
             sender=email,
-            recipients=["seanohioroberts@gmail.com"]
+            recipients=["seanohioroberts@gmail.com"],
         )
-        Mail.send(msg)
+        mail.send(msg)
+        return redirect("/contact")
     else:
         return render_template("contact.html")
 
