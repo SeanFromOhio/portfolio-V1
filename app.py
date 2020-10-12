4803fbd0ceac9b9ee2eca322c7573961a35b16ba
SeanFromOhio <seanohioroberts@gmail.com>
2020-10-12 15:30:37 -0400

    Resume download button now works. Contact Me page now constructed and is close to completion utilizing flask mail library. Fixed issue with navbar Projects button not linking correctly.

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_mail import Mail, Message
+
 app = Flask(__name__)
+mail = Mail(app)
 
 
 @app.route("/")
@@ -12,9 +15,23 @@ def resume():
     return render_template("resume.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        subject = request.form["subject"]
+        message = request.form["message"]
+
+        msg = Message(
+            message,
+            subject="*Portfolio*" + name + "-" + subject,
+            sender=email,
+            recipients=["seanohioroberts@gmail.com"]
+        )
+        Mail.send(msg)
+    else:
+        return render_template("contact.html")
 
 
 if __name__ == "__main__":
